3aae8c3aa9bc6edb337fc7aea980a0632e6ab1ef
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-23 00:04:48 +0530

    Welcome to Linked list Programs

---
+++ Linkedlist-Day14/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Linkedlist_Day14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcocme to Linked List Programs");
+        }
+    }
+}
