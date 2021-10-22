0a425d478a55a02c701c7ce0a0fc19043d9ffd38
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-23 01:13:38 +0530

    adding program

--- Linkedlist-Day14/Program.cs
+++ Linkedlist-Day14/Program.cs
@@ -7,9 +7,11 @@ namespace Linkedlist_Day14
         static void Main(string[] args)
         {
             LinkedList list = new LinkedList();
-            list.Add(56);
-            list.Add(30);
             list.Add(70);
+            list.Display();
+            list.Add(30);
+            list.Display();
+            list.Add(56);
             list.Display();
 
         }
